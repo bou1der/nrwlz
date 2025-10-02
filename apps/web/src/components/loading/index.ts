5d740334090b80b2a2ee75c6bb6a12a1d2aae478
bou1der <krowkin@bk.ru>
2025-10-02 13:23:52 +0300

    Add loader spinner

--- apps/web/src/components/loading/index.ts
+++ apps/web/src/components/loading/index.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
 
 @Component({
+  imports: [MatProgressSpinner],
   selector: "app-loader",
   templateUrl: "./index.html",
   styleUrl: "./index.scss",
